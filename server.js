08ee322c3fa3ccc952b3a5a46ed40e6690a0adf1
Sadie Amato <saamato@email.unc.edu>
2023-03-21 18:03:06 -0400

    Fix package.json typo in test script

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get("/app/rpsls/play/:shot", (req, res) => {
   res.status(200).send(rpsls(req.params.shot));
 });
 
-// For undefined endpoints
+// For all other undefined endpoints
 app.get("*", (req, res) => {
     res.status(404).send("404 NOT FOUND");
   });
